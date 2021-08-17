08cba3f0fa6a9bb397f7aeacf9ca995907198808
Sean <sd.cotter45@gmail.com>
2021-08-17 12:21:35 -0400

    calendar date ranges

--- front-end/src/components/PostPage/HostInfo/index.js
+++ front-end/src/components/PostPage/HostInfo/index.js
@@ -14,10 +14,15 @@ function HostInfo({post}) {
         ]
     })
 
+    const blockDates = (disabledDates) => {
+
+    }
+
     console.log(disabledDates)
 
-    const [value, onChange] = useState(new Date());
+    const [value, onChange] = useState();
 
+    // console.log(value[0].toDateString())
     return (
         <div className='info'>
             <div className='host'>
@@ -35,7 +40,21 @@ function HostInfo({post}) {
                 <Calendar
                     onChange={onChange}
                     value={value}
+                    selectRange
+                    disabledDates
                     />
+                {value && (
+                    <div className='selected-dates'>
+                        <div className='start'>
+                            <h3>Start</h3>
+                            <p>{value[0].toDateString()}</p>
+                        </div>
+                        <div className='end'>
+                            <h3>End</h3>
+                            <p>{value[1].toDateString()}</p>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
 

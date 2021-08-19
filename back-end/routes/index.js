1cd57c6585819fea9a23e63ff45d28049c7f9fd5
Sean <sd.cotter45@gmail.com>
2021-08-18 20:30:40 -0400

    heroku again

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -18,25 +18,25 @@ if (process.env.NODE_ENV !== 'production') {
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
-    // Serve the front-end's index.html file at the root route
-    router.get('/', (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.sendFile(
-        path.resolve(__dirname, '../../../front-end', 'build', 'index.html')
-      );
-    });
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../front-end', 'build', 'index.html')
+    );
+  });
 
-    // Serve the static assets in the front-end's build folder
-    router.use(express.static(path.resolve("../../../front-end/build")));
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../front-end/build")));
 
-    // Serve the front-end's index.html file at all other routes NOT starting with /api
-    router.get(/^(?!\/?api).*/, (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.sendFile(
-        path.resolve(__dirname, '../../front-end', 'build', 'index.html')
-      );
-    });
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../front-end', 'build', 'index.html')
+    );
+  });
 }
 
 

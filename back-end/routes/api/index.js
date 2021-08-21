d618c51a11cbed1e4504859685ba3fe6d2228a23
Sean <sd.cotter45@gmail.com>
2021-08-21 19:51:45 -0400

    heroku

--- back-end/routes/api/index.js
+++ back-end/routes/api/index.js
@@ -5,8 +5,8 @@ const usersRouter = require('./users.js');
 const postsRouter = require('./posts.js');
 const mapsRouter = require('./maps');
 
-router.use('/maps', mapsRouter);
 router.use('/session', sessionRouter);
+router.use('/maps', mapsRouter);
 router.use('/users', usersRouter);
 router.use('/posts', postsRouter);
 
